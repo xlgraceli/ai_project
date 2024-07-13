bfd264f616a13cc7e07088456dd102993653dd29
xlgraceli <xlgraceli@gmail.com>
2024-07-12 21:50:04 -0400

    server created

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json({ limit: '10mb' }));
+
+// Handles image upload
+app.post('/api/upload', (req, res) => {
+    const { image } = req.body;
+    console.log('Received image:', image);
+    res.json({ message: 'Image received successfully!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- src/components/Camera.js
+++ src/components/Camera.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import Webcam from 'react-webcam';
+import axios from 'axios';
 import './Camera.css';
 
 const Camera = () => {
-    return (
-      <div>
-        <Webcam 
-          className="mirrored"
-          audio={false} 
-          screenshotFormat="image/jpeg" 
-          width="640" 
-          height="480" 
-        />
-      </div>
-    );
+  const webcamRef = useRef(null);
+
+  const capture = async () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    if (imageSrc) {
+      await sendImageToApi(imageSrc);
+    }
   };
+
+  const sendImageToApi = async (imageData) => {
+    try {
+      const response = await axios.post('http://localhost:5000/api/upload', { image: imageData });
+      console.log('API Response:', response.data);
+    } catch (error) {
+      console.error('Error sending image to API:', error);
+    }
+  };
+
+  return (
+    <div>
+
+      <Webcam className="mirrored" audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+      <button onClick={capture}>Capture and Send</button>
+    </div>
+  );
+};
   
   export default Camera;
   
